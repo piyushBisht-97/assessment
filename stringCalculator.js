a67ba6298c1032a490b5103a9f70f87810017ec5
piyushBisht-97 <piyushbisht250@gmail.com>
2025-04-02 12:00:52 +0530

    fixed delimitter and negative string issue

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,12 +1,19 @@
- function stringCalculator(numbers){
-    const separtors = /,|\n/
+function stringCalculator(numbers){
+if(!numbers) return 0
 
-    const data = numbers.split(separtors).map((item)=>Number(item))
-    const check = data.filter((item)=>item<0)
-    if(check.length){
-        return "negative numbers not allowed"
-    }
-    const result = data.reduce((acc,curr) =>acc+curr,0)
+    let delimiter = /,|;|\n/;
+   if (numbers.startsWith("//")) {
+       const parts = numbers.split("\n")
+       delimiter = new RegExp(parts[0].substring(2))
+       numbers = parts.slice(1).join("\n")
+   }
+    console.log("asdf",numbers)
+   const data = numbers.split(delimiter).map((item)=>Number(item))
+   const check = data.filter((item)=>item<0)
+   if(check.length){
+    throw new Error(`Negative numbers not allowed: ${check.join()}`);
+}
+   const result = data.reduce((acc,curr) =>acc+curr,0)
 return result    
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,14 +4,15 @@ test("Will retun 0 if input is an empty string ",()=>{
     expect(stringCalculator("")).toBe(0)
 })
 
-test("Will return negative numbers not allowed if input is negative number",()=>{
-    expect(stringCalculator("1,-5")).toBe("negative numbers not allowed")
-})
+test("Will return 'negative numbers not allowed' if input contains a negative number", () => {
+    expect(() => stringCalculator("1,-5")).toThrow("Negative numbers not allowed");
+});
+
 
 test("Should return sum of input string",()=>{
     expect(stringCalculator("1,7")).toBe(8)
 })
 
 test("Supports multiple delimiter",()=>{
-    expect(stringCalculator("//;\n1;2").toBe(3))
+    expect(stringCalculator("//;\n1;2")).toBe(3)
 })
